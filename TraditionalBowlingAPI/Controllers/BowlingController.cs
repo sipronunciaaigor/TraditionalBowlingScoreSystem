35b359d94f4b9770548ada0bbef1990823d4c64a
Fabrizio <fabrizio.dipietro@hotmail.it>
2021-08-15 14:21:59 +1000

    Default boilerplate + init

---
+++ TraditionalBowlingAPI/Controllers/BowlingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraditionalBowlingServices;
+
+namespace TraditionalBowlingScoreSystem.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BowlingController : ControllerBase
+{
+    private readonly ILogger<BowlingController> _logger;
+
+    public BowlingController(ILogger<BowlingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        List<int> input = new() { 1, 1, 1 };
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            Console.Write(string.Concat(i + 1, " [" + input[i] + "] -"));
+        }
+        Console.WriteLine(Environment.NewLine);
+
+        var frames = ScoringService.Frames.GetFrames(input);
+        for (int i = 0; i < frames.Count; i++)
+        {
+            Console.Write(string.Concat(i + 1, " [" + string.Join(',', frames[i]) + "] -"));
+        }
+
+        var scores = ScoringService.Scores.GetScores(input, frames);
+        Console.WriteLine(string.Concat(Environment.NewLine, string.Join(',', scores)));
+
+        return Ok(scores);
+    }
+}
---
+++ TraditionalBowlingServices/ScoringService.cs
@@ -0,0 +1,96 @@
+﻿
+namespace TraditionalBowlingServices;
+
+
+// 1. find frames
+// 2. analyse each frame
+public class ScoringService
+{
+    public class Frames
+    {
+        public static List<List<int>> GetFrames(List<int> input)
+        {
+            List<List<int>> frames = new();
+            List<int> shots = new();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                shots.Add(input[i]);
+                int sum = shots.Sum();
+
+                // take 2 shots
+                if (sum == 10 || shots.Count == 2)
+                {
+                    frames.Add(shots.ToList());
+                    shots.Clear();
+                }
+                // take 1 shot
+                else if (sum > 10)
+                {
+                    frames.Add(shots.Take(shots.Count - 1).ToList());
+                    shots = new(input[i]);
+                }
+
+                // on 9th take all that's left
+                if (frames.Count == 9)
+                {
+                    frames.Add(input.Skip(i).ToList());
+                    shots.Clear();
+                    break;
+                }
+            }
+            return frames;
+        }
+    }
+
+    public class Scores
+    {
+        public static List<string> GetScores(List<int> input, List<List<int>> frames)
+        {
+            List<string> labels = new();
+            var score = 0;
+            var index = 0;
+
+            for (var i = 0; i < frames.Count; i++)
+            {
+                var frameSum = frames[i].Sum();
+                var frameLen = frames[i].Count;
+                index += frameLen;
+
+                if (frameLen == 1 && frameSum == 10)
+                {
+                    var nextTwo = input.Skip(index).Take(index + 2).ToList();
+                    score += 10 + nextTwo.Sum();
+                    labels.Add(nextTwo.Count < 2 ? "*" : score.ToString());
+                    continue;
+                }
+                if (frameLen == 1 && frameSum < 10)
+                {
+                    score += frameSum;
+                    labels.Add("*");
+                    continue;
+                }
+                if (frameLen == 2 && frameSum == 10)
+                {
+                    var nextOne = input.Skip(index).Take(index + 1).ToList();
+                    score += 10 + nextOne.Sum();
+                    labels.Add(nextOne.Count < 1 ? "*" : score.ToString());
+                    continue;
+                }
+                if (frameLen == 2 && frameSum < 10)
+                {
+                    score += frames[i].Sum();
+                    labels.Add(score.ToString());
+                    continue;
+                }
+                if (i == frames.Count - 1 && frameLen < 4 && frameSum <= 30)
+                {
+                    score += frames[i].Sum();
+                    labels.Add(score.ToString());
+                    continue;
+                }
+            }
+            return labels;
+        }
+    }
+}
