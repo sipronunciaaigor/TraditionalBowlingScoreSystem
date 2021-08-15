b5c9775efc0a0b07bc132fa2763ad38f5cdb60f2
fabrizio <fabrizio@edapp.com>
2021-08-15 22:29:42 +1000

    Moving strategies

--- TraditionalBowlingServices/ScoringService.cs
+++ TraditionalBowlingServices/ScoringService.cs
@@ -93,58 +93,68 @@ public class ScoringService
         public static GameProgressResponseDto GetScores(List<int> pinsDowned, List<List<int>> frames)
         {
             List<string> labels = new();
-            var score = 0;
-            var index = 0;
+            int score = 0;
+            int index = 0;
 
             for (var i = 0; i < frames.Count; i++)
             {
-                var frameSum = frames[i].Sum();
-                var frameLen = frames[i].Count;
+                int frameSum = frames[i].Sum();
+                int frameLen = frames[i].Count;
                 index += frameLen;
 
-                // strike frame strategy
-                if (frameLen == 1 && frameSum == 10)
-                {
-                    var nextTwo = pinsDowned.Skip(index).Take(2).ToList();
-                    score += 10 + nextTwo.Sum();
-                    labels.Add(nextTwo.Count < 2 ? "*" : score.ToString());
-                    continue;
-                }
-                // open frame strategy
-                if (frameLen == 1 && frameSum < 10)
-                {
-                    score += frameSum;
-                    labels.Add("*");
-                    continue;
-                }
-                // spare frame strategy
-                if (frameLen == 2 && frameSum >= 10)
-                {
-                    var nextOne = pinsDowned.Skip(index).Take(1).ToList();
-                    score += 10 + nextOne.Sum();
-                    labels.Add(nextOne.Count < 1 ? "*" : score.ToString());
-                    continue;
-                }
-                // closed frame strategy
-                if (frameLen == 2 && frameSum < 10)
-                {
-                    score += frameSum;
-                    labels.Add(score.ToString());
-                    continue;
-                }
-                // last frame strategy
-                if (i == frames.Count - 1 && frameLen < 4 && frameSum <= 30)
-                {
-                    score += frameSum;
-                    labels.Add(score.ToString());
-                    continue;
-                }
-
-
-                // error strategy
-                labels.Add("err");
+                score = ExecuteStrategy(
+                    (frameLen == 1 && frameSum == 10, () => StrikeStrategy(pinsDowned, index, score, labels)),
+                    (frameLen == 1 && frameSum < 10, () => OpenStrategy(score, frameSum, labels)),
+                    (frameLen == 2 && frameSum >= 10, () => SpareStrategy(pinsDowned, index, score, labels)),
+                    (frameLen == 2 && frameSum < 10, () => ClosedStrategy(score, frameSum, labels)),
+                    (i == frames.Count - 1 && frameLen < 4 && frameSum <= 30, () => LastStrategy(score, frameSum, labels))
+                );
             }
             return new GameProgressResponseDto() { FrameProgressScores = labels };
+        }
+        
+        public static int ExecuteStrategy(params(bool condition, Func<int> strategy)[] strategies)
+        {
+            var t = strategies.First(tuple => tuple.condition);
+            var r = t.strategy.Invoke();
+            return r;
+        }
+
+        public static int LastStrategy(int score, int frameSum, List<string> labels)
+        {
+            score += frameSum;
+            labels.Add(score.ToString());
+            return score;
+        }
+
+        public static int ClosedStrategy(int score, int frameSum, List<string> labels)
+        {
+            score += frameSum;
+            labels.Add(score.ToString());
+            return score;
+        }
+
+        public static int OpenStrategy(int score, int frameSum, List<string> labels)
+        {
+            score += frameSum;
+            labels.Add("*");
+            return score;
+        }
+
+        public static int SpareStrategy(List<int> pinsDowned, int index, int score, List<string> labels)
+        {
+            var nextOne = pinsDowned.Skip(index).Take(1).ToList();
+            score += 10 + nextOne.Sum();
+            labels.Add(nextOne.Count < 1 ? "*" : score.ToString());
+            return score;
+        }
+
+        public static int StrikeStrategy(List<int> pinsDowned, int index, int score, List<string> labels)
+        {
+            var nextTwo = pinsDowned.Skip(index).Take(2).ToList();
+            score += 10 + nextTwo.Sum();
+            labels.Add(nextTwo.Count < 2 ? "*" : score.ToString());
+            return score;
         }
     }
 }
